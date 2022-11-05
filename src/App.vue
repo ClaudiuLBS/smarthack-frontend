dccf033e219dd9ae21dacd3a740b9582f8150173
VladWero08 <vlad_wero@yahoo.com>
2022-11-05 14:09:19 +0200

    Default VueApp

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <home-header></home-header>
+</template>
+
+<script>
+import HomeHeader from './views/HomeHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomeHeader
+  }
+}
+</script>
---
+++ src/views/HomeHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="big-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+    <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit saepe eaque quam veritatis eum. Consequuntur?</div>
+</template>
+
+<script>
+import '../assets/CSS/reset-sheet.css';
+import '../assets/CSS/root.css';
+
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
